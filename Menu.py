419df53a9e4481c7f6dfaa6901618169780f2f8e
rareba <rareba2ttu.ee>
2016-12-25 17:58:13 +0200

    Random font.

--- Menu.py
+++ Menu.py
@@ -1,15 +1,18 @@
 """Menu."""
 
 import pygame
+import random
+import KeyListener
 import Main
 
 pygame.init()
 
-#  print(pygame.font.get_fonts())  prints a string of all the available fonts
+
+#  print(pygame.font.get_fonts()) # prints a string of all the available fonts
 
 
 class Menu:
-    def __init__(self, scrn, buttons, bg_color=(192, 192, 192), font="chiller", font_size=120, text_color=(0, 0, 0)):
+    def __init__(self, scrn, buttons, bg_color=(192, 192, 192), font=random.choice(pygame.font.get_fonts()), font_size=60, text_color=(0, 0, 0)):
         self.scrn = scrn  # screen
         self.scr_width = self.scrn.get_rect().width  # width of the screen
         self.scr_height = self.scrn.get_rect().height  # height of the screen
@@ -24,13 +27,14 @@ class Menu:
             text_width, text_height = text.get_rect().width, text.get_rect().height
             posx = (self.scr_width / 2) - (text_width / 2)
             posy = (self.scr_height / 2) - (len(buttons) * text_height / 2) + (i * text_height)
-
             self.buttons.append([button, text, (posx, posy)])
 
     def running(self):
         while True:
-            print(pygame.mouse.get_pos())
+            #  KeyListener.update()
+            #  print(KeyListener.mouseX, KeyListener.mouseY)
             self.scrn.fill(self.bg_color)
+            pygame.event.get()  # so it wouldn't crash, delete events
             for name, text, (posx, posy) in self.buttons:
                 self.scrn.blit(text, (posx, posy))  # draw a source surface onto this screen
             pygame.display.flip()
