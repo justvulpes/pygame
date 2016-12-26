ac3090fb038ee2ab8ef7989f95a41caf32899232
mapuut <mapuut@ttu.ee>
2016-12-26 02:39:25 +0200

    b02

--- objects/projectiles/Projectile.py
+++ objects/projectiles/Projectile.py
@@ -2,20 +2,24 @@ import objects.Object
 import World
 import math
 class Projectile(objects.Object.Object):
-    def __init__(self, x, y, end_x, end_y, sprite, size=10, speed=1):
+    def __init__(self, x, y, end_x, end_y, sprite, size=10, speed=1, lifetime=100):
         super().__init__(x,y)
         self.angle = math.atan2(end_y - y, end_x - x)
         self.sprite = sprite
         self.size = size
         self.speed = speed
-        self.xSpeed = self.speed * math.cos(self.angle);
-        self.ySpeed = self.speed * math.sin(self.angle);
+        self.lifetime = lifetime
+        self.xSpeed = self.speed * math.cos(self.angle)
+        self.ySpeed = self.speed * math.sin(self.angle)
 
     def update(self):
+        self.lifetime -= 1
+        if self.lifetime < 0:
+            World.World.objects.remove(self)
         self.move(self.xSpeed, self.ySpeed)
 
     def render(self, display):
-        display.canvas.blit(self.sprite.pic, (self.x - World.World.camera_x - self.sprite.width / 2, self.y - World.World.camera_y - self.sprite.height / 2 - 4))
+        display.canvas.blit(self.sprite.pic, (self.x - World.World.camera_x - self.sprite.width / 2, self.y - World.World.camera_y - self.sprite.height / 2))
 
     def move(self, x, y):
         if x != 0 and y != 0:
--- objects/projectiles/Stone.py
+++ objects/projectiles/Stone.py
@@ -3,5 +3,5 @@ import game_graphics.Sprite
 
 class Stone(objects.projectiles.Projectile.Projectile):
     def __init__(self, x, y, end_x, end_y):
-        super().__init__(x, y, end_x, end_y, game_graphics.Sprite.stone_projectile, size=10, speed=1,)
+        super().__init__(x, y, end_x, end_y, game_graphics.Sprite.stone_projectile, size=10, speed=10, lifetime=15)
 
